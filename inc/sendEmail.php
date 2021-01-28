2464be38c58e2793f67ddb398a6fb8e676ccd921
mreed6 <reed3@oakland.edu>
2021-01-28 12:43:05 -0500

    Testing email contact page

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -4,27 +4,29 @@
 $siteOwnersEmail = 'msreed3@outlook.com';
 
 
-if($_POST) {
+if(!empty($_POST)) {
 
-   $name = trim(stripslashes($_POST['contactName']));
-   $email = trim(stripslashes($_POST['contactEmail']));
-   $subject = trim(stripslashes($_POST['contactSubject']));
-   $contact_message = trim(stripslashes($_POST['contactMessage']));
+   $name = $_POST['contactName'];
+   $email = $_POST['contactEmail'];
+   $subject = $_POST['contactSubject'];
+   $contact_message = $_POST['contactMessage'];
 
    // Check Name
-	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+	if (empty($name)) {
+		$error[] = "Please enter your name.";
 	}
 	// Check Email
-	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
+	if (empty($email)) {
+		$error[] = "Please enter a valid email address.";
 	}
 	// Check Message
-	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+	if (empty($contact_message)) {
+		$error[] = "Please enter your message. It should have at least 15 characters.";
 	}
    // Subject
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+	if (empty($subject == '')) {
+	    $empty[] = "Contact Form Submission";
+	}
 
 
    // Set Message
